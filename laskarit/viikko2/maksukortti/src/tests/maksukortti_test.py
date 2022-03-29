ee7d3c8ed138129891a145e274e7ecf21a943e7a
Ellen <ellen.rajala@gmail.com>
2022-03-29 16:20:51 +0300

    muutoksia

--- laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
+++ laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
@@ -5,5 +5,10 @@ class TestMaksukortti(unittest.TestCase):
     def setUp(self):
         print("Set up goes here")
 
-    def test_hello_world(self):
-        self.assertEqual("Hello world", "Hello world")
+    def test_konstruktori_asettaa_saldon_oikein(self):
+        kortti = Maksukortti(10)
+
+        vastaus = str(kortti)
+
+        self.assertEqual(vastaus, "Kortilla on rahaa 10 euroa")
+
