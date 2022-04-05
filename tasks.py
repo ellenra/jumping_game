76d3dbf7c2a4dd02983b1a4f8834405e7e5ca870
ellenra <102103873+ellenra@users.noreply.github.com>
2022-04-05 22:55:14 +0300

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -7,4 +7,11 @@ def coverage(ctx):
 @task(coverage)
 def coverage_report(ctx):
     ctx.run("coverage html", pty=True)
+    
+@task
+def start(ctx):
+    ctx.run("python3 src/index.py", pty=True)
 
+@task
+def test(ctx):
+    ctx.run("pytest src", pty=True)
